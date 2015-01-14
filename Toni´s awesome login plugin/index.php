bdaa28724a0a241866f45bde1dca698de4e1d6ad
Hirmuli <omena_n@hotmail.com>
2015-01-14 13:06:44 +0200

    Useless functions removed
    Fixed username capture and removed useless functions.

--- Toni´s awesome login plugin/index.php
+++ Toni´s awesome login plugin/index.php
@@ -17,61 +17,54 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
 */
+
 defined('ABSPATH') or die("No script kiddies please!"); //Blocks direct access to plugins PHP files.
 
 date_default_timezone_set('UTC'); //set the default timezone to use. Available PHP 5.1
-
- 
 //runs the plugin when user tries to login
 add_action ('wp_signon', 'get_login_information');
 
+//main fuction to create user info
 function get_login_information()
 {
 // check if "login was successful or failed
 if ( 0 == $current_user -> ID ) {
-		//Action on failed login
-		add_action ( 'wp_login_failed', 'login_failed', 10, 2 );
-}
-else {
-		//Action on successful login
-		add_action ( 'wp_login', 'login_failed', 10, 2 );
-		}
+	//Action on failed login
+	//get current user info
+	get_currentuserinfo();
+	//ad attmpted username to variable
+	$username = $current_user->user_login;
+	//ad string login FAILED to login
+	$login = 'login FAILED';
+	//save time to variable
+	$date_time = date("D M j G:i:s T Y");
+	//ad userinformation to variable
+	$user_info = $username . "," . $date_time . "," . $login;
 
-}
-
-function login_failed ($user_login, $user)
-{		
-		//save user_login string to username
-		$username = $user_login;
-		//ad string login FAILED to login
-		$login = 'login FAILED';
-		//save time to variable
-		$date_time = date("D M j G:i:s T Y");
-		//ad userinformation to variable
-		$user_info = $username . "," . $date_time . "," . $login;
-		//save information to log
-		save_to_log($user_info);
+	save_to_log($user_info);
 }
 
-function login_success ($user_login, $user)
-{
-		//save user_login string to username
-		$username = $user_login;
-		//ad string "login SUCCESS" to login
-		$login = 'login SUCCESS';
-		//save time to variable
-		$date_time = date("D M j G:i:s T Y");
-		//pass user information to  one variable
-		$user_info = $username . "," . $date_time . "," . $login;
-		//save information to log
-		save_to_log($user_info);
-	
+	else {
+	//Action on successful login
+	//get current user info
+	get_currentuserinfo();
+	//ad attempted username to variable
+	$username = $current_user->user_login;
+	//ad string login FAILED to login
+	$login = 'login SUCCESS';
+	//save time to variable
+	$date_time = date("D M j G:i:s T Y");
+	//ad userinformation to variable
+	$user_info = $username . "," . $date_time . "," . $login;
+
+	save_to_log($user_info);
+	}
 }
 
 function save_to_log($user_info){
-fputs($fp, $user_info."\n"); // write the data in the opened file
-$fp = fopen('login.log', a);
-fclose($fp); // close the filer/wordpress/account/".TEMPLATEPATH. $file, $user_info, FILE_APPEND);
- }
- 
+	$fp = fopen('login.log', a); 
+	fputs($fp, $user_info."\n"); // write the data in the opened file
+	fclose($fp); // close the filer/wordpress/account/".TEMPLATEPATH. $file, $user_info, FILE_APPEND);
+}
 ?>
+
